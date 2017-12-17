e6b886e8ad496a74ee5b85accca30a092dc01be8
codinglilong <791373142@qq.com>
2017-12-17 21:48:08 +0800

    周日提交

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,4 +1,17 @@
+require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-var navSide = require('page/common/nav-side/index.js');
-navSide.init({name:'user-center'});
+require('util/slider/index.js');
+var templateBanner =require('./index.template');
+var _mm=require('util/mm.js');
+$(function() {
+    var bannerHtml = _mm.renderHtml(templateBanner,{});
+    $('.banner-con').html(bannerHtml);
+    var $slider = $('.banner').unslider({
+        dots: true,
+    });
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev')?'prev':'next';
+        $slider.data('unslider')[forward]();
+    });
+});
